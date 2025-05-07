a53256b0d7bde35d4d4a9b7bcd70ddaf811cd790
Ibrahim Jamil <ibrahimjamil090@gmail.com>
2025-05-07 09:23:20 -0700

    initial commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,34 @@
-import './Home.css'
-import { useContext, useState } from 'react'
-import Posts from '../../components/Posts'
-import Sidebar from '../../components/Sidebar'
-import useWindowDimensions from '../../hooks/useWindowDimensions'
-import { AuthContext } from '../../context/AuthContext'
-import Navbar from '../../components/Navbar'
+import "./Home.css";
+import { useContext, useState } from "react";
+import Posts from "../../components/Posts";
+import Sidebar from "../../components/Sidebar";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+import { AuthContext } from "../../context/AuthContext";
+import Navbar from "../../components/Navbar";
 
 const Home = () => {
-  const { width } = useWindowDimensions()
-  const [isPostsOpen, setIsPostsOpen] = useState(true)
-  const [isFriendsOpen, setIsFriendsOpen] = useState(true)
-  const { user } = useContext(AuthContext)
+  const { width } = useWindowDimensions();
+  const [isPostsOpen, setIsPostsOpen] = useState(true);
+  const [isFriendsOpen, setIsFriendsOpen] = useState(true);
+  const { user } = useContext(AuthContext);
 
   const displayPosts = () => {
-    setIsPostsOpen(true)
-    setIsFriendsOpen(false)
-  }
+    setIsPostsOpen(true);
+    setIsFriendsOpen(false);
+  };
 
   const displayFriends = () => {
-    setIsFriendsOpen(true)
-    setIsPostsOpen(false)
-  }
+    setIsFriendsOpen(true);
+    setIsPostsOpen(false);
+  };
 
   if (width < 680) {
-    if (isPostsOpen) if (isFriendsOpen) setIsFriendsOpen(false)
+    if (isPostsOpen) if (isFriendsOpen) setIsFriendsOpen(false);
   }
 
   if (width > 680) {
-    if (!isPostsOpen) setIsPostsOpen(true)
-    if (!isFriendsOpen) setIsFriendsOpen(true)
+    if (!isPostsOpen) setIsPostsOpen(true);
+    if (!isFriendsOpen) setIsFriendsOpen(true);
   }
 
   return (
@@ -36,11 +36,11 @@ const Home = () => {
       <Navbar onClickPosts={displayPosts} onClickFriends={displayFriends} />
       <div className="container">
         {isPostsOpen && <Posts />}
-        {isFriendsOpen && user.role === 'user' && <Sidebar />}
+        {isFriendsOpen && user.role === "user" && <Sidebar />}
       </div>
-      <footer className="footer">Copyright © 2022 Michał Osman</footer>
+      <footer className="footer">Copyright © 2022 Muhammad Ibrahim</footer>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
